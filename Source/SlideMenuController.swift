9d8c18f9d506a523c7f6f46a3e656e5327cf8712
Duy <admin@Admins-MacBook-Pro-2.local>
2017-10-31 23:13:34 +0700

    edit width

--- Source/SlideMenuController.swift
+++ Source/SlideMenuController.swift
@@ -19,7 +19,7 @@ import UIKit
 }
 
 public struct SlideMenuOptions {
-    public static var leftViewWidth: CGFloat = 375
+    public static var leftViewWidth: CGFloat = UIScreen.main.bounds.width
     public static var leftBezelWidth: CGFloat? = 16.0
     public static var contentViewScale: CGFloat = 0.96
     public static var contentViewOpacity: CGFloat = 1.0
